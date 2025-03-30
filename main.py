812c1e4725df1f18988c682874a75f1c0b8ac583
skukucka98 <skukucka98@gmail.com>
2025-03-30 15:32:36 -0400

    added stats for chars

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from stats import get_num_words
+from stats import get_character_count
 
 def main():
-    print(f"{get_num_words()} words found in the document")
+    num_words = get_num_words()
+    char_dict = get_character_count()
+    print(f"{num_words} words found in the document")
+    print(char_dict)
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,14 @@ def get_book_text():
 def get_num_words():
     num_words = len(get_book_text().split())
     return num_words
+
+def get_character_count():
+    count_dict = {}
+    lower_text = get_book_text().lower()
+    char_list = list(lower_text)
+    for char in char_list:
+        if char not in count_dict:
+            count_dict[char] = 1
+        else:
+            count_dict[char] += 1
+    return count_dict
