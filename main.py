6874deecb7f195135e3a2ca329b317f5f6cb3e1d
skukucka98 <skukucka98@gmail.com>
2025-03-31 11:47:20 -0400

    finished bookbot

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from stats import get_num_words
-from stats import get_character_count
+from stats import get_sorted_alpha_chars
+import sys
 
 def main():
-    num_words = get_num_words()
-    char_dict = get_character_count()
-    print(f"{num_words} words found in the document")
-    print(char_dict)
+    args = sys.argv
+
+    if len(args) == 2:
+        num_words = get_num_words(args[1])
+        sorted_char_dict = get_sorted_alpha_chars(args[1])
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {args[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for dict in sorted_char_dict:
+            print(f"{dict["char"]}: {dict["num"]}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
--- stats.py
+++ stats.py
@@ -1,16 +1,16 @@
-def get_book_text():
+def get_book_text(path_to_book):
     file_contents = None
-    with open("books/frankenstein.txt") as f:
+    with open(path_to_book) as f:
         file_contents = f.read()    
     return file_contents
 
-def get_num_words():
-    num_words = len(get_book_text().split())
+def get_num_words(path_to_book):
+    num_words = len(get_book_text(path_to_book).split())
     return num_words
 
-def get_character_count():
+def get_character_count(path_to_book):
     count_dict = {}
-    lower_text = get_book_text().lower()
+    lower_text = get_book_text(path_to_book).lower()
     char_list = list(lower_text)
     for char in char_list:
         if char not in count_dict:
@@ -18,3 +18,18 @@ def get_character_count():
         else:
             count_dict[char] += 1
     return count_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_alpha_chars(path_to_book):
+    char_dict = get_character_count(path_to_book)
+    sorted_char_list = []
+    for char in char_dict:
+        if str(char).isalpha():
+            dict = {}
+            dict["char"] = char
+            dict["num"] = char_dict[char]
+            sorted_char_list.append(dict)
+    sorted_char_list.sort(reverse=True, key=sort_on)
+    return sorted_char_list
